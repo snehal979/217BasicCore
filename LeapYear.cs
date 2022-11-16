d3ef00f9c4f5dde37faa8c0d61c4b15b109a6e3a
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-16 21:07:08 +0530

    check leap year

---
+++ LeapYear.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblem
+{
+    public class LeapYear
+    {
+        public static void CheckLeapYear(int year)
+        {
+            
+            if (year.ToString().Length == 4)
+            {
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine("{0} is the leap year", year);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not leap year", year);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter 4 digit leap year");
+            }
+        }
+    }
+}
