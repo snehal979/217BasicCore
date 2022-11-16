165023a2d8ebe8961078eee922b055694548a929
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-16 21:06:27 +0530

    find the large number

---
+++ LargerNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblem
+{
+    internal class LargerNum
+    {
+        public static void LargenNumber(int num1,int num2,int num3)
+        {
+            
+
+            if (num1>num2)
+            {
+                if (num1>num3)
+                {
+                    Console.WriteLine("{0} is greater number than another two number", num1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is greater number than another two number", num3);
+                }
+
+            }
+            else if (num2>num3)
+            {
+                Console.WriteLine("{0} is greater number than another two number", num2);
+            }
+
+        }
+    }
+}
