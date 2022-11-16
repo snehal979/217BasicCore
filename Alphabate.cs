45e237b28e4abe8f08ac72a45d4d69b5c661fbc3
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-16 21:04:09 +0530

    compare alphabate with vowel

---
+++ Alphabate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblem
+{
+    internal class Alphabate
+    {
+        public static void VowelAndConsonant()
+        {
+            Console.WriteLine("enter the alphabet A-Z and a-z");
+            char ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(ch);
+            // a, e, i, o, u 
+            if (ch == 'a' || ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u' || ch == 'A' ||ch == 'E' ||ch == 'I' ||ch == 'O' ||ch == 'U')
+            {
+                Console.WriteLine("{0} is vowel alphabet", ch);
+            }
+            else
+            {
+                Console.WriteLine("{0} alphabet is consonant", ch);
+            }
+        }
+    }
+}
